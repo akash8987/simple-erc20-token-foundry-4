75f31e5f4b17b7a6e12c30f944041b2ac84fc4b8
Akash <161524961+akash8987@users.noreply.github.com>
2026-02-14 13:38:20 +0600

    Create MyToken.sol

---
+++ MyToken.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import {ERC20} from "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
+
+/**
+ * @title MyToken
+ * @dev Implementation of a basic ERC20 Token.
+ */
+contract MyToken is ERC20 {
+    constructor(uint256 initialSupply) ERC20("MyToken", "MTK") {
+        _mint(msg.sender, initialSupply);
+    }
+}
